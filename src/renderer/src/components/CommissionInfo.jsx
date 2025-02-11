ae989af3987a32b6eaed30398a7e79012cc628d2
rlim2 <usprinterhihi1@gmail.com>
2025-02-10 21:17:09 -0800

    titlebar fixed frfr plz dont break

---
+++ src/renderer/src/components/CommissionInfo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './commissionInfo.module.css'
+
+const CommissionInfo = () => {
+
+    return (
+        <div className={styles.container}>
+            
+        </div>
+    )
+}
+
+export default CommissionInfo
--- src/renderer/src/components/Home.jsx
+++ src/renderer/src/components/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import CardContainer from './CardContainer.jsx'
 import styles from './home.module.css'
+import CommissionInfo from './CommissionInfo.jsx'
 
 const Home = () => {
 
     return (
         <div className={styles.container}>
+            <CommissionInfo />
             <CardContainer />
         </div>
     )
