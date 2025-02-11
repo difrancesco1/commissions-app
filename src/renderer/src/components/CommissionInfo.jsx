d13ab8d075f9ada08b57a8cbe295c2fed8fa601c
rlim2 <usprinterhihi1@gmail.com>
2025-02-10 23:38:04 -0800

    wow

--- src/renderer/src/components/CommissionInfo.jsx
+++ src/renderer/src/components/CommissionInfo.jsx
@@ -1,11 +1,35 @@
+/* eslint-disable prettier/prettier */
 import React from 'react'
 import styles from './commissionInfo.module.css'
+import content from '../../../content.js'
 
 const CommissionInfo = () => {
 
     return (
-        <div className={styles.container}>
-            
+        <div className={styles.commissionInfo}>
+            <div className={styles.clientInfo}>
+                <div className={styles.imgStyle}></div>
+                <div className={styles.clientText}>
+                    <p className={styles.nameText}>Lavana/Lana✩</p>
+                    <p className={styles.noteText}>add new face</p>
+                    <p className={styles.subText}>@Lavanai_</p>
+                    <p className={styles.subText}>cloudyyrachel@gmail.com</p>
+                    <p className={styles.subText}>paypal@gmail.com</p>
+                </div>
+                <div className={styles.dueInfo}>
+                    <p className={styles.subText}>2/10</p>
+                    <p className={styles.subText}>A02</p>
+                </div>
+            </div>
+            <div className={styles.clientInfo}>
+                <div className={styles.emailBtn}>$</div>
+                <div className={styles.emailBtn}>+</div>
+                <div className={styles.emailBtn}>$+</div>
+                <div className={styles.emailBtn}>✎</div>
+                <div className={styles.emailBtn}>✿</div>
+                <div className={styles.emailBtn}>✉</div>
+            </div>
+            <div></div>
         </div>
     )
 }
