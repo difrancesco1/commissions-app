5848066be88f138d688f3a16f4beba8accbfd2d8
difrancesco1 <joshiqua@gmail.com>
2025-02-12 14:55:32 -0800

    new buttons

--- src/renderer/src/components/FooterBtn.jsx
+++ src/renderer/src/components/FooterBtn.jsx
@@ -1,6 +1,9 @@
 /* eslint-disable prettier/prettier */
 import React from 'react'
 import styles from './footerBtn.module.css'
+import refreshgmail from '../../../assets/refreshgmail.png'
+import search from '../../../assets/search.png'
+import copytocarrd from '../../../assets/copytocarrd.png'
 
 const FooterBtn = () => {
     const handleClick = () => {
@@ -9,12 +12,18 @@ const FooterBtn = () => {
 
   return (
     <div className={styles.footerContainer} >
-      <div className={styles.boxButton}>↺</div>
+      <div className={styles.boxButton}>
+        <img className={styles.buttonText} src={refreshgmail} alt="refreshgmail button"/>
+      </div>
 
       <div className={styles.searchBar}>-.⊹˖ᯓ★. ݁₊</div>
-      <div className={styles.rectangleButton}>↵</div>
+      <div className={styles.rectangleButton}>
+        <img className={styles.buttonText} src={search} alt="search button"/>
+      </div>
 
-      <div className={styles.boxButton}>●</div>
+      <div className={styles.boxButton}>
+        <img className={styles.buttonText} src={copytocarrd} alt="copytocarrd button"/>
+      </div>
     </div>
   )
 }
