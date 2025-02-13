84cc2008eea867fa662fadd2a0204b4e430e4b12
josh difrancesco <joshiqua@gmail.com>
2025-02-13 02:08:43 -0800

    images on cards

--- src/renderer/src/components/Card.jsx
+++ src/renderer/src/components/Card.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import styles from "./card.module.css";
-
+import loading from "../../../assets/loading.gif";
 import { db } from "../firebaseConfig";
 import {
   collection,
@@ -11,18 +11,17 @@ import {
   doc,
 } from "firebase/firestore";
 
-import loading from "../../../assets/loading.gif";
-
+// fetches data from Firestore
 const fetchDataFromFirestore = async () => {
-  // const querySnapshot = await getDocs(collection(db, "commissions"));
-  const sectionsCollectionRef = collection(db, "commissions");
-  const q = query(
+  const sectionsCollectionRef = collection(db, "commissions"); // gets "commissions" collection from DB
+  const q = query(               // Creates query that orders the docs by: ARCHIVE, PAID, and DUE
     sectionsCollectionRef,
     orderBy("ARCHIVE"),
-    orderBy("PAID", "desc"),
-    orderBy("DUE"),
+    orderBy("PAID", "desc"),     // Descending Order
+    orderBy("DUE")
   );
-  const querySnapshot = await getDocs(q);
+  // snapshot iterates through the fetched docs and extracts their data
+  const querySnapshot = await getDocs(q); // Fetches the docs based on our query
 
   const data = [];
   querySnapshot.forEach((doc) => {
@@ -31,71 +30,87 @@ const fetchDataFromFirestore = async () => {
   return data;
 };
 
-const Card = ({ user, setCommissionIndex }) => {
-  const [data, setData] = useState([]);
 
+const Card = ({ user, setCommissionIndex }) => {
+  const [data, setData] = useState([]); //data store the fetched data 
+  const [imageExists, setImageExists] = useState(false); // imageExists stores whether an image exists or not 
+
+  const imagePath = `http://localhost:5000/API/images/${user.ID}.png`; // URL pointing towards server API endpoint
+
+  // fetch image from the imagePath
+  const loadImage = async () => {
+    try {
+      const imageCheck = await fetch(imagePath);
+      if (imageCheck.ok) {
+        setImageExists(true); // Image exists, set state to true
+      } else {
+        setImageExists(false); // Image doesn't exist, set state to false
+      }
+    } catch (error) {
+      setImageExists(false); // Error handling: set state to false if image fetch fails
+    }
+  };
+
+  // when user.ID changes
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchData = async () => {                   // Calls db to set new data
       const newData = await fetchDataFromFirestore();
       setData(newData);
     };
 
-    fetchData();
-  }, []);
+    fetchData(); 
+    loadImage(); // Checks if the image exists by calling the loadImage function
+  }, [user.ID]); // [user.ID] ensures that the effect will ONLY run when user.ID changes ^_^
 
-  const contextMenu = document.getElementById("contextMenu");
-
-  const handleClick = (id) => {
-    setCommissionIndex(id);
-    handleCloseMenu;
-    console.log(id);
+  //When user clicks on card
+  const handleClick = (id) => { 
+    setCommissionIndex(id); // sets commission index
+    handleCloseMenu(); // closes context menu
   };
 
+  // Toggles the PAID status of a commission in DB
   const togglePaid = async () => {
-    try {
+    try { 
       const documentRef = doc(db, "commissions", user.id);
       await updateDoc(documentRef, {
         ["PAID"]: `${!user.PAID}`,
       });
-      console.log("paid toggled!");
+      console.log("paid status toggled :PP");
     } catch (error) {
-      console.error("Error toggling paid:", error);
+      console.error("Error toggling de paid status:", error);
     }
   };
 
+  // Toggles Archive Status
   const toggleArchive = async () => {
     try {
       const documentRef = doc(db, "commissions", user.id);
       await updateDoc(documentRef, {
         ["ARCHIVE"]: `${!user.ARCHIVE}`,
       });
-      console.log("archive toggled!");
+      console.log("archive status toggled :PP");
     } catch (error) {
-      console.error("Error toggling archive:", error);
+      console.error("Error toggling de archive:", error);
     }
   };
 
   // right click handling
+  // menuVisible hides and shows context menu
   const [menuVisible, setMenuVisible] = useState(false);
+  // Stores the mouse coords when right-clicking
   const [mouseX, setMouseX] = useState(0);
   const [mouseY, setMouseY] = useState(0);
-
+  //Prevents default rightclick menu
   const handleContextMenu = (event) => {
-    // event.preventDefault();
-    // console.log("Right click event triggered");
-    // contextMenu.style.display = "flex";
     event.preventDefault();
     setMouseX(event.clientX);
     setMouseY(event.clientY);
     setMenuVisible(true);
   };
-
+  // Hides context menu when triggered
   const handleCloseMenu = () => {
     setMenuVisible(false);
   };
-
-  // TODO: CHANGE THIS TO ACTUALLY BE LOGICAL ^_^
-  const imageExists = false;
 
   return (
     <div
@@ -105,38 +120,40 @@ const Card = ({ user, setCommissionIndex }) => {
     >
       <div
         key={user.id}
-        className={`
-          ${user.ARCHIVE === true ? styles.cardArchive : null}
-          ${imageExists === true ? styles.card : styles.loadingCardStyle}`}
+        className={`${
+          user.ARCHIVE === true ? styles.cardArchive : null
+        } ${imageExists ? styles.card : styles.loadingCardStyle}`}
         onClick={() => handleClick(user.id)}
         onContextMenu={handleContextMenu}
       >
         <img
-          className={`${imageExists === true ? styles.image : styles.loadingStyle}`}
-          src={`${imageExists === true ? user.IMG1 : loading}`} // TODO: CHANGE USER.IMG1
+          className={`${imageExists ? styles.image : styles.loadingStyle}`}
+          src={imageExists ? imagePath : loading}
           alt={user.NAME}
         />
       </div>
-      {/* <div id="contextMenu" className={styles.wrapper}>
-        <ul>
-          <li className={styles.item}>$</li>
-          <li className={styles.item}>▾</li>
-        </ul>
-      </div> */}
+
       <div onContextMenu={handleContextMenu} className={styles.wrapper}>
         {menuVisible && (
           <div onClick={handleCloseMenu} onMouseLeave={handleCloseMenu}>
             <ul>
-              <li className={styles.item} onClick={togglePaid}>
-                $
-              </li>
-              <li className={styles.item} onClick={toggleArchive}>
-                ▾
-              </li>
+            <li className={styles.item} onClick={() => {
+              console.log("Toggling Paid");
+              togglePaid();
+            }}>
+              $
+            </li>
+            <li className={styles.item} onClick={() => {
+              console.log("Toggling Archive");
+              toggleArchive();
+            }}>
+              ▾
+            </li>
             </ul>
           </div>
         )}
       </div>
+
       <h1
         className={`${styles.cardText} 
           ${user.PAID === true ? null : styles.textNotPaid} 
