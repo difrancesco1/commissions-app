12f7d06dbce311edca4bf6ba6f07e8197cff678c
difrancesco1 <joshiqua@gmail.com>
2025-02-12 18:29:37 -0800

    buttons stay same place even if no commissioner info

--- src/renderer/src/components/CommissionInfo.jsx
+++ src/renderer/src/components/CommissionInfo.jsx
@@ -58,7 +58,7 @@ const CommissionInfo = ({ commissionIndex, searchQuery }) => {
         )}
       </div>
 
-      <div className={styles.clientInfo}>
+      <div className={styles.emailButtonContainer}>
         <div className={styles.emailBtn}>
           <img className={styles.buttonText} src={btn1} alt="didntpayemailbutton"/>
         </div>
